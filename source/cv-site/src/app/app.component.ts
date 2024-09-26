52078ffe74428c6e97be63adfbd5e088188d14cc
Montefiori Adéna Auróra <adena.montefiori@gmail.com>
2024-09-26 22:56:54 +0200

    Metro page

--- source/cv-site/src/app/app.component.ts
+++ source/cv-site/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
+  currentRoute: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  getClassForCurrentRoute(): string {
+    return this.currentRoute.replace('/', '');
+  }
 }
