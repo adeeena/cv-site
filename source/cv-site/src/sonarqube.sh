c67be217db337946540df572f31bdcf26fdcc6d6
Adéna Auróra Muráncsik <adenam@silversea.com>
2025-09-01 11:05:34 +0200

    chore: removed unnecessary sonar entries

--- source/cv-site/src/sonarqube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sonar-scanner \
-  -Dsonar.projectKey=CVPage \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_775614fabaf391172fff60e7b217a57d84a459c6
